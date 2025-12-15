c23f1e0331f2e2b5b722a98490acdfb201075644
I-am-Lavakusha <lavakumarhn578@gmail.com>
2025-12-15 18:15:26 +0530

    15-12-2025

---
+++ OneDrive/Desktop/code/Day-01/simple_array_sum.py
@@ -0,0 +1,6 @@
+def simpleArraySum(ar):
+    return sum(ar)
+    
+array_input = [7, 5, 2, 10, 1]
+result = simpleArraySum(array_input)
+print(result) 
---
+++ OneDrive/Desktop/code/Day-01/solve_me_first.py
@@ -0,0 +1,7 @@
+def solveMeFirst(a,b):
+  return a + b
+
+num1 = 15
+num2 = 8
+result = solveMeFirst(num1, num2)
+print(result) 
