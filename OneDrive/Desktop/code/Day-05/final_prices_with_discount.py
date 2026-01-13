6701b5d6676b177ff26297f6e4c633488fcb7dc2
I-am-Lavakusha <lavakumarhn578@gmail.com>
2026-01-13 17:47:20 +0530

    this is a easy program

---
+++ OneDrive/Desktop/code/Day-05/final_prices_with_discount.py
@@ -0,0 +1,14 @@
+def final_prices(prices):
+    res = []
+    for i in range(len(prices)):
+        discount = 0
+        for j in range(i+1, len(prices)):
+            if prices[j] <= prices[i]:
+                discount = prices[j]
+                break
+        res.append(prices[i] - discount)
+    return res
+
+# Example
+prices = [8,4,6,2,3]
+print(final_prices(prices))  # [4, 2, 4, 2, 3]
