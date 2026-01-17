faac15aa7bd0503551fdb97344a2e2aa34647a07
I-am-Lavakusha <lavakumarhn578@gmail.com>
2026-01-17 22:28:24 +0530

    this is the program to understand and create user defined module

---
+++ OneDrive/Desktop/code/100_days_of_coding/Day-04/random/my_module.py
@@ -0,0 +1,4 @@
+#this is how we can can create our own module and then we can use in our programs by using module_name.variable_name or module_name.method_name
+#i will use this variable in my random.py program.
+
+my_variable="no work"
