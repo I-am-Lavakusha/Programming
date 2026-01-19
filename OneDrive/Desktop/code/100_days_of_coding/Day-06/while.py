bc560827d3054aec49c5af554189c6739ff63475
I-am-Lavakusha <lavakumarhn578@gmail.com>
2026-01-19 22:47:51 +0530

    this is to demonstrate while loops

---
+++ OneDrive/Desktop/code/100_days_of_coding/Day-06/while.py
@@ -0,0 +1,13 @@
+#this will help you to understand the funtions and while loop
+#first let's create our user defined funtion
+
+#def <function_name>():
+  #statements
+
+#functions will be executed when we call them
+
+
+def my_function():
+  print("jello")
+
+my_function()
