f9646fbe3ec3c402ebf0d1fbacc3d285cc23fd93
I-am-Lavakusha <lavakumarhn578@gmail.com>
2026-01-21 23:34:52 +0530

    this is the simple module of the functions

---
+++ OneDrive/Desktop/code/100_days_of_coding/Day-08/greet.py
@@ -0,0 +1,18 @@
+# def greet():
+#   print("Hello lava")
+#   print("How are you")
+#   print("good to see you")
+
+# greet()
+
+# def greet_with_name(name):
+#   print(f"Hello {name}")
+#   print(f"how do u do {name}")
+
+# greet_with_name("lavakumar")
+
+def greet_with(name, location):
+  print(f"Hello {name}")
+  print(f"what is it like in {location}")
+
+greet_with(location="chennai",name="lava")
