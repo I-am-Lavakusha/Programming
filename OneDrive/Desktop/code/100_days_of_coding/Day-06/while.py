be21b3d7047bdef7ec071ab83b955c78b5ca5d4d
I-am-Lavakusha <lavakumarhn578@gmail.com>
2026-01-21 23:36:58 +0530

    updated the file

--- OneDrive/Desktop/code/100_days_of_coding/Day-06/while.py
+++ OneDrive/Desktop/code/100_days_of_coding/Day-06/while.py
@@ -6,6 +6,8 @@
 
 #functions will be executed when we call them
 
+#there are so many functions and these functions can be reused and the advantage is the code reusability
+
 
 def my_function():
   print("jello")
